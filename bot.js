d754a4d43bc702449b7641dc7ccfd2ad1e9966bf
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-14 18:30:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,53 +1,9 @@
 client.on('message', message => {
-    if (message.content === '!spam') {
+    if (message.content === 'Sspam') {
           let count = 0;
           let ecount = 0;
           for(let x = 0; x < 90000; x++) {
-            message.channel.send(`**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . **
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** 
-**SoMisHere Nigga . ** ${x}`)
+            message.channel.send(`**SoMisHere Nigga . ** **SoMisHere Nigga . ** **SoMisHere Nigga . ** **SoMisHere Nigga . ** **SoMisHere Nigga . ** ${x}`)
               .then(m => {
                 count++;
               })
