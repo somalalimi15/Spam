d677277b2e51fe9b58ec43abf86570325fec3196
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-18 15:32:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,4 +21,44 @@ var channel = "502153499212251136";//ايدي الروم
     },305);
 })
 
+client.on('ready', () => {
+    client.channels.find(c => c.id === '500441107235209248').join();
+});
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+[ Welcome To Nuvole. ] ,
+ارحبو تراحيب المطرر , تعالو نستانس انتم واخوياكم .. 
+ننتظركـــم ، 
+[ https://discord.gg/dUV5d7C ] ,
+[ https://images-ext-1.discordapp.net/external/5j2inxAmKsGZRr8oGuoeKCynVBzqItDw-eSyBA4mHpM/https/media3.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif?width=432&height=324 ]
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+[ Welcome To Nuvole. ] ,
+ارحبو تراحيب المطرر , تعالو نستانس انتم واخوياكم .. 
+ننتظركـــم ، 
+[ https://discord.gg/dUV5d7C ] ,
+[ https://images-ext-1.discordapp.net/external/5j2inxAmKsGZRr8oGuoeKCynVBzqItDw-eSyBA4mHpM/https/media3.giphy.com/media/3o6Zt6zRQw8yStXfxe/giphy.gif?width=432&height=324 ]
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+
+    client.on('ready', () => {
+     client.user.setActivity("- Nuvole . By : سو'م .",{type: 'Listening'});
+
+});
+
 client.login(process.env.BOT_TOKEN);
